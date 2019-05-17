6a55fc8b39b35d4b7a862eea15de7366158cc72b
Sherjil999 <42275702+Sherjil999@users.noreply.github.com>
2019-05-16 21:13:52 -0400

    Update GroceryList.java

--- ArrayList/src/GroceryList.java
+++ ArrayList/src/GroceryList.java
@@ -9,7 +9,7 @@ public class GroceryList {
 	}
 	
 	public void printGroceryList() {
-		System.out.println("You have " + groceryList.size() + " items in your grocery list");
+		System.out.println("You have " + groceryList.size() + " items in your grocery list");//
 		
 		for(int i = 0; i < groceryList.size(); i++) {
 			System.out.println((i + 1) + "." + groceryList.get(i));
